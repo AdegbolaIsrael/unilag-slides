0f2c71423d0cef42a60254b332107afcf1ba8293
Horlakz <horlakz@protonmail.com>
2022-02-21 13:56:54 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Unilag Slides
+This is a platform for students to be able to get slides from the platform at no cost
+
+Helps student that are looking for resources to learn
+
+## Features
+- slides are going to be from a google drive
+- It should have sections for each courses
+- a lot pages
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <main>
+        <h1>
+          We are going to be building a forum for unilag for students to be able
+          to get slides from the forum easily
+        </h1>
+        <p>so Izzy and Yomi, get ready to work!</p>
+      </main>
+    </>
+  );
+}
+
+export default App;
